61cf648dd7869dcb4cd0a6514385545244791fe5
Artem Petrikov <manforten@tandex.ru>
2023-04-10 00:19:17 +0300

    JavaDoc comments for AppController

--- src/main/java/com/example/tickets/AppController.java
+++ src/main/java/com/example/tickets/AppController.java
@@ -4,28 +4,45 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Controller; // Позволяет указать адрес страницы
-import org.springframework.ui.Model; // Интерфейс, который необходим для взаимодействия Controller и конфигуратора modelviewcontroller, а также для добавления всех элементов web интерфейса
+import org.springframework.stereotype.Controller; // Allows to specify the page address
+import org.springframework.ui.Model; // The interface that is necessary for the interaction of the Controller and the modelviewcontroller configurator, as well as for adding all elements of the web interface
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView; // Метод позволяющий указать название html страниц, которые мы подвязываем к нашему модулю
+import org.springframework.web.servlet.ModelAndView; // A method that allows you to specify the name of the html pages that we link to our module
 
-
+/**
+ * Controller for the application
+ * @author Artem Petrikov
+ */
 @Controller
 public class AppController {
+
 
     private final TicketsService service;
 
     private final FlightsService service2;
 
-
+    /**
+     * Method for calling services
+     * @param service is a TicketsService for interacting with tickets
+     * @param service2 is a FlightsService for interacting with flights
+     */
     public AppController(TicketsService service, FlightsService service2) {
         this.service = service;
         this.service2 = service2;
     }
-    @Value("${STRIPE_PUBLIC_KEY}")
-    private String stripePublicKey;
 
-    @RequestMapping("/") // "/" - означает, что будет открываться главная страница
+    @Value("${STRIPE_PUBLIC_KEY}")
+    private String stripePublicKey; // The public key that is required for payment
+
+
+    /**
+     * The method required to display the main page
+     * @param model required to add attributes
+     * @param keyword this parameter from the TicketsService class is required to implement the search
+     * @param keyword2 this parameter from the FlightService class is required to implement the search
+     * @return index.html with all parameters
+     */
+    @RequestMapping("/") // "/" - means that the main page will open
     public String viewHomePage(Model model, @Param("keyword") String keyword, @Param("keyword2") String keyword2){
         List<Tickets> listTickets = service.listAll(keyword);
         model.addAttribute("listTickets", listTickets);
@@ -33,13 +50,17 @@ public class AppController {
         List<Flights> listFlights = service2.listAll(keyword2);
         model.addAttribute("listFlights", listFlights);
         model.addAttribute("keyword2", keyword2);
-        model.addAttribute("amount", 50 * 100); // in cents
-        model.addAttribute("stripePublicKey", stripePublicKey);
-        model.addAttribute("currency", "USD");
-        return "index"; // Возвращение html страницы
+        return "index"; // Returning the html page
     }
 
-    @RequestMapping("/user") // "/" - означает, что будет открываться главная страница
+    /**
+     * The method required to display the user page
+     * @param model required to add attributes
+     * @param keyword this parameter from the TicketsService class is required to implement the search
+     * @param keyword2 this parameter from the FlightService class is required to implement the search
+     * @return index.html with all parameters
+     */
+    @RequestMapping("/user") // "/user" - means that the user page will open
     public String viewUserPage(Model model, @Param("keyword") String keyword, @Param("keyword2") String keyword2){
         List<Tickets> listTickets = service.listAll(keyword);
         model.addAttribute("listTickets", listTickets);
@@ -47,111 +68,178 @@ public class AppController {
         List<Flights> listFlights = service2.listAll(keyword2);
         model.addAttribute("listFlights", listFlights);
         model.addAttribute("keyword2", keyword2);
-        return "user"; // Возвращение html страницы
+        return "user"; // Returning the html page
     }
 
-    @RequestMapping("/new") // Контроллер по добавлению билета
+    /**
+     * Controller for adding new ticket
+     * @param model required to add attributes
+     * @return new_tickets.html with all parameters
+     */
+    @RequestMapping("/new")
     public String showNewTicketsForm(Model model){
         Tickets tickets = new Tickets();
-        model.addAttribute("tickets", tickets); // Первый параметр название database
-        return "new_tickets"; // Возвращение html страницы
+        model.addAttribute("tickets", tickets); // The first parameter is the name of the database
+        return "new_tickets"; // Returning the html page
     }
 
+    /**
+     * Controller for saving ticket data
+     * @param tickets required to add attributes to the database
+     * @return the transfer to the user page
+     */
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveTickets(@ModelAttribute("tickets") Tickets tickets){
         service.save(tickets);
-        return "redirect:/user"; // Сохранение и перенос на главную страницу
+        return "redirect:/user"; // Saving and transferring to the main page
     }
 
+    /**
+     * Controller for editing a ticket by id
+     * @param id required to accurately indicate the attribute that needs to edit
+     * @return page with ticket data by id
+     */
     @RequestMapping("/edit/{id}")
-    public ModelAndView showEditTicketsForm(@PathVariable(name = "id") Long id) { // id - параметр из браузерной строки
+    public ModelAndView showEditTicketsForm(@PathVariable(name = "id") Long id) { // id - parameter from the browser line
         ModelAndView mav = new ModelAndView("edit_tickets");
         Tickets tickets = service.get(id);
         mav.addObject("tickets", tickets);
-        return mav; // Возвращаем страницу с данными о книге по id
+        return mav; // Returning the page with the ticket data by id
     }
 
+    /**
+     * Controller for deleting a ticket by id
+     * @param id required to accurately indicate the attribute that needs to delete
+     * @return the transfer to the user page
+     */
     @RequestMapping("/delete/{id}")
     public String deleteTickets(@PathVariable(name = "id") Long id){
         service.delete(id);
-        return "redirect:/user"; // Сохранение и перенос на главную страницу
+        return "redirect:/user"; // Saving and transferring to the user page
     }
 
-    @RequestMapping("/admin") // "/" - означает, что будет открываться главная страница
+    /**
+     * Controller for displaying the admin page
+     * @param model required to add attributes
+     * @param keyword2 this parameter from the FlightService class is required to implement the search
+     * @return admin.html with all parameters
+     */
+    @RequestMapping("/admin")
     public String viewAdminPage(Model model, @Param("keyword2") String keyword2){
         List<Flights> listFlights = service2.listAll(keyword2);
         model.addAttribute("listFlights", listFlights);
         model.addAttribute("keyword2", keyword2);
-        return "admin"; // Возвращение html страницы
+        return "admin"; // Returning the html page
     }
 
-
-    @RequestMapping("/new2") // Контроллер по добавлению билета
+    /**
+     * Controller for adding new flight
+     * @param model required to add attributes
+     * @return new_flights.html with all parameters
+     */
+    @RequestMapping("/new2")
     public String showNewFlightsForm(Model model){
         Flights flights = new Flights();
-        model.addAttribute("flights", flights); // Первый параметр название database
-        return "new_flights"; // Возвращение html страницы
+        model.addAttribute("flights", flights); // The first parameter is the name of the database
+        return "new_flights"; // Returning the html page
     }
 
+    /**
+     * Controller for saving flight data
+     * @param flights required to add attributes to the database
+     * @return the transfer to the admin page
+     */
     @RequestMapping(value = "/save2", method = RequestMethod.POST)
     public String saveFlights(@ModelAttribute("flights") Flights flights){
         service2.save(flights);
-        return "redirect:/admin"; // Сохранение и перенос на главную страницу
+        return "redirect:/admin"; // Saving and transferring to the admin page
     }
 
+    /**
+     * Controller for editing a flight by id
+     * @param id required to accurately indicate the attribute that needs to edit
+     * @return page with flight data by id
+     */
     @RequestMapping("/edit2/{id}")
-    public ModelAndView showEditFlightsForm(@PathVariable(name = "id") Long id) { // id - параметр из браузерной строки
+    public ModelAndView showEditFlightsForm(@PathVariable(name = "id") Long id) { // id - parameter from the browser line
         ModelAndView mav = new ModelAndView("edit_flights");
         Flights flights = service2.get(id);
         mav.addObject("flights", flights);
-        return mav; // Возвращаем страницу с данными о книге по id
+        return mav; // Returning the page with the ticket data by id
     }
 
+    /**
+     * Controller for deleting a flight by id
+     * @param id required to accurately indicate the attribute that needs to delete
+     * @return the transfer to the admin page
+     */
     @RequestMapping("/delete2/{id}")
     public String deleteFlights(@PathVariable(name = "id") Long id){
         service2.delete(id);
-        return "redirect:/admin"; // Сохранение и перенос на главную страницу
+        return "redirect:/admin"; // Saving and transferring to the admin page
     }
 
-    @RequestMapping("/histogram") // Контроллер по открытию гистограммы
+    /**
+     * Controller for opening the page with a histogram
+     * @param model required to add attributes
+     * @param keyword2 this parameter from the FlightService class is necessary to declare the table through which the histogram will be created
+     * @return histogram.html with all parameters
+     */
+    @RequestMapping("/histogram")
     public String showNewHistogramForm(Model model, @Param("keyword") String keyword2){
         List<Flights> listFlights = service2.listAll(keyword2);
         model.addAttribute("listFlights", listFlights);
         model.addAttribute("keyword2", keyword2);
-        return "histogram"; // Возвращение html страницы
-    }
-    @RequestMapping("/author")
-    public String showAuthorForm(){
-        return "author"; // Возвращение html страницы
+        return "histogram"; // Returning the html page
     }
 
+    /**
+     * Controller for opening the page about the author
+     * @return author.html with all parameters
+     */
+    @RequestMapping("/author")
+    public String showAuthorForm(){
+        return "author"; // Returning the html page
+    }
+
+    /**
+     * Controller for saving data about the name and surname of the person who is booking a ticket
+     * @param tickets required to add attributes to the database
+     * @return the transfer to the payment page
+     */
     @RequestMapping(value = "/save3/{id}", method = RequestMethod.POST)
     public String savePayTickets(@ModelAttribute("tickets") Tickets tickets){
         service.save(tickets);
-        return "redirect:/payment/{id}"; // Сохранение и перенос на главную страницу
+        return "redirect:/payment/{id}"; // Saving and transferring to the payment page
     }
 
+    /**
+     * Controller to go to the page for filling in the name and surname to buy the ticket
+     * @param id required to accurately indicate the attribute that needs to buy
+     * @return page with ticket data by id
+     */
     @RequestMapping("/edit3/{id}")
-    public ModelAndView showEditPayTicketsForm(@PathVariable(name = "id") Long id) { // id - параметр из браузерной строки
+    public ModelAndView showEditPayTicketsForm(@PathVariable(name = "id") Long id) { // id - parameter from the browser line
         ModelAndView mav = new ModelAndView("edit_pay");
         Tickets tickets = service.get(id);
         mav.addObject("tickets", tickets);
-        return mav; // Возвращаем страницу с данными о книге по id
+        return mav; // Returning the page with the ticket data by id
     }
+
+    /**
+     * Controller for displaying the ticket payment page
+     * @param model required to add attributes
+     * @param id required to accurately indicate the attribute that was purchased
+     * @return page with ticket data by id
+     */
     @RequestMapping("/payment/{id}")
-    public ModelAndView showPaymentForm(Model model, @PathVariable(name = "id") Long id){
+    public ModelAndView showPaymentForm(Model model, @PathVariable(name = "id") Long id){ // id - parameter from the browser line
         ModelAndView mav = new ModelAndView("payment");
         Tickets tickets = service.get(id);
         mav.addObject("tickets", tickets);
-        model.addAttribute("amount", tickets.getPrice()); // in cents
-        model.addAttribute("stripePublicKey", stripePublicKey);
-        model.addAttribute("currency", ChargeRequest.Currency.EUR);
-        return mav; // Возвращаем страницу с данными о книге по id
+        model.addAttribute("amount", tickets.getPrice()); // Measured in cents
+        model.addAttribute("stripePublicKey", stripePublicKey); // PublicKey for payment
+        model.addAttribute("currency", ChargeRequest.Currency.EUR); // Choice of currency
+        return mav; // Returning the page with the ticket data by id
     }
-
-
-
-
-
-
 }
