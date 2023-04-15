6a46162d74aa9e75729772e2a9de6f629f83f4d0
Artem Petrikov <manforten@tandex.ru>
2023-04-15 20:43:52 +0300

    JavaDoc comments for TicketsManagerApplication

--- src/main/java/com/example/tickets/TicketsManagerApplication.java
+++ src/main/java/com/example/tickets/TicketsManagerApplication.java
@@ -2,14 +2,15 @@ package com.example.tickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Class for running a web application
+ * @author Artem Petrikov
+ */
 @SpringBootApplication
 public class TicketsManagerApplication extends SpringBootServletInitializer{
     public static void main(String[] args) {
-        SpringApplication.run(TicketsManagerApplication.class, args); // Запуск web приложения
+        SpringApplication.run(TicketsManagerApplication.class, args); // Launching a web application
     }
 }
