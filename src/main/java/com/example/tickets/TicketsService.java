8852fdbe85fe6c9867ad86c902784e689454f75c
Artem Petrikov <manforten@tandex.ru>
2023-04-15 20:53:09 +0300

    JavaDoc comments for TicketsService

--- src/main/java/com/example/tickets/TicketsService.java
+++ src/main/java/com/example/tickets/TicketsService.java
@@ -1,34 +1,60 @@
 package com.example.tickets;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+
+/**
+ * This class represents a service that performs various operations on data.
+ * It includes methods for search, save, get and deleting data.
+ * @author Artem Petrikov
+ */
 @Service
 public class TicketsService {
 
     private final TicketsRepository repo;
 
+    /**
+     * Method for calling service
+     * @param repo is a TicketsRepository for interacting with tickets
+     */
     public TicketsService(TicketsRepository repo) {
         this.repo = repo;
     }
 
-    public List<Tickets> listAll(String keyword){ //Коллекция и метод отвечающая за поиск и фильтр в нашей системе
-        if (keyword != null){ // Если ключевое слово = null, то вызываем метод из первого return (вывод всех значений)
+    /**
+     * Collection and method responsible for search and filter in the system
+     * @param keyword this parameter from the TicketsService class is required to implement the search
+     * @return If the keyword = null, then call the method from the first return (output all values), otherwise nothing will be output
+     */
+    public List<Tickets> listAll(String keyword){
+        if (keyword != null){
             return repo.search(keyword);
         }
-        return repo.findAll(); // Ничего не выведется
+        return repo.findAll();
     }
 
+    /**
+     * Method for saving
+     * @param tickets required to add attributes to the database
+     */
     public void save(Tickets tickets){
         repo.save(tickets);
     }
 
+    /**
+     * Method for getting data by id
+     * @param id required to accurately indicate the attribute that needs to show
+     * @return data by id
+     */
     public Tickets get(Long id){
         return repo.findById(id).get();
     }
 
+    /**
+     * Method for deleting data by id
+     * @param id required to accurately indicate the attribute that needs to delete
+     */
     public void delete(Long id){
         repo.deleteById(id);
     }
