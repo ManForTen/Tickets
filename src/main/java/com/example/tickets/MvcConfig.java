321d94f61641b4483aa84b97172e9d267c3b18c1
Artem Petrikov <manforten@tandex.ru>
2023-04-15 21:59:45 +0300

    JavaDoc comments for MvcConfig

--- src/main/java/com/example/tickets/MvcConfig.java
+++ src/main/java/com/example/tickets/MvcConfig.java
@@ -1,12 +1,20 @@
 package com.example.tickets;
 
-import org.springframework.context.annotation.Configuration; // Аннотация для объединения методов в одно springboot приложение
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry; // Аннотация для объединения методов в одно springboot приложение
+import org.springframework.context.annotation.Configuration; // Annotation for combining methods into a single springboot application
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry; // Annotation for combining methods into a single springboot application
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for Spring MVC.
+ * @author Artem Petrikov
+ */
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    /**
+     * Allows users to access the login page
+     * @param registry is the ViewControllerRegistry object used to login the view controller.
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/login").setViewName("login");
