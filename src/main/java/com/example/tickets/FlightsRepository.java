3db621e83dbf1977cb8878f28c2e0ea4600b91a5
Artem Petrikov <manforten@tandex.ru>
2023-04-15 20:38:42 +0300

    JavaDoc comments for FlightsRepository

--- src/main/java/com/example/tickets/FlightsRepository.java
+++ src/main/java/com/example/tickets/FlightsRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The interface that is needed to search for flights by aircraft model, number of available seats and ticket price
+ * @author Artem Petrikov
+ */
 @Repository
 public interface FlightsRepository extends JpaRepository<Flights, Long> {
-    @Query("SELECT p FROM Flights p WHERE CONCAT(p.aircraft_model, ' ', p.count_free_seats, ' ', p.price) LIKE %?1%")
+    @Query("SELECT p FROM Flights p WHERE CONCAT(p.aircraft_model, ' ', p.count_free_seats, ' ', p.price, ' ', p.price_b) LIKE %?1%")
     List<Flights> search(String keyword2);
 }
